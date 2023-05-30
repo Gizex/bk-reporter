6b588434855d4211cad566304ff3ca9fba289721
Gizex <admin@gizex.ru>
2023-05-30 13:11:35 +0300

    init commit

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ for line in lines:
     parts = line.split("-")
     if len(parts) >= 3:
         line = "-".join(parts[1:3])
-        if "qa" in line.lower():
-            if "prod" in line.lower():
-                line = "PROD-INFRA"
-            elif "dev" in line.lower():
-                line = "DEV-INFRA"
     trimmed_lines.append(line.upper())
 
 with open('trimmed_data.txt', 'w') as file:

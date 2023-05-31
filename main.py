f7efd39848dceb73937393d7af46071a58f1514b
Gizex <admin@gizex.ru>
2023-05-31 09:30:28 +0300

    upd

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ trimmed_lines = []
 for line in lines:
     parts = line.split("-")
     if len(parts) >= 3:
+        if parts[1] == "stage":
+            parts[1] = "dev"
+        elif "qa" in line.lower():
+            if "prod" in line.lower():
+                line = "PROD-INFRA"
+            elif "dev" in line.lower():
+                line = "DEV-INFRA"
         line = "-".join(parts[1:3])
     trimmed_lines.append(line.upper())
 

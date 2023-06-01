106616f208626c4cf7e2f61892b2c08373b4b9e0
Gizex <admin@gizex.ru>
2023-06-01 15:06:30 +0300

    добавил сводную таблицу

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-
+import datetime
 # Читаем исходный файл в память с заменой разделителя
 data = []
 with open('data-export.csv', 'r', encoding='utf-8') as csv_in:
@@ -22,11 +22,11 @@ for row in data[1:]:
     if len(parts) >= 3:
         if parts[1] in ['stage', 'preprod', 'int']:
             parts[1] = 'dev'
-        if parts[2] in ['qa', 'monitoring', 'test', 'template', 'ci', 'stpage']:
+        if parts[2] in ['qa', 'monitoring', 'test', 'template', 'ci', 'stpage', 'cmdb']:
             parts[2] = 'infra'
         if parts[2] in ['ml', 'superset', 'recsys']:
             parts[2] = 'analytics'
-        if parts[2] in ['stoplist', 'userv', 'static']:
+        if parts[2] in ['stoplist', 'userv', 'static', 'cadmin']:
             parts[2] = 'mpback'
         if parts[2] == 'ksk':
             parts[2] = 'kiosk'
@@ -42,8 +42,32 @@ for row in data[1:]:
 # Sort filtered data by 'Project'
 filtered_data.sort(key=lambda x: x[0])
 
-# Write filtered data to new file
-with open('data-export-filtered.csv', 'w', newline='', encoding='utf-8') as csv_out:
-    writer = csv.writer(csv_out, delimiter=';')
-    writer.writerow(new_cols)  # write new headers
-    writer.writerows(filtered_data)  # write filtered data
+
+
+# Получаем текущую дату
+current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+# Получаем первое слово из столбца 'Name' для формирования названия файла
+first_word = filtered_data[3][2].split('-')[0]
+
+# Формируем имя файла с текущей датой и первым словом из столбца 'Name'
+filename = f"{first_word} Отчет о потреблении ресурсов {current_date}.csv"
+
+import pandas as pd
+
+# Create dataframe from your filtered data
+df = pd.DataFrame(filtered_data, columns=new_cols)
+
+# Ensure the numeric columns are treated as such
+df[['CPUs', 'Memory MB', 'Storage MB']] = df[['CPUs', 'Memory MB', 'Storage MB']].apply(pd.to_numeric)
+
+# Create pivot table
+pivot_df = df.pivot_table(index='Project', 
+                          values=['CPUs', 'Memory MB', 'Storage MB'], 
+                          aggfunc='sum')
+
+# Save as an excel file with two sheets, one with original data and the second with the pivot table
+with pd.ExcelWriter(f"{first_word} Отчет о потреблении ресурсов {current_date}.xlsx", engine='openpyxl') as writer:
+    df.to_excel(writer, sheet_name='Original Data', index=False)
+    pivot_df.to_excel(writer, sheet_name='Pivot Table')
+
